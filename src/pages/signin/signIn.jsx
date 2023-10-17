4a3715f6410872f5d4d351e3cf0d72d357b6d000
puncho-iv <unicusdnl@gmail.com>
2023-10-17 06:49:57 +0100

    <chore: responsivess>: startup page completed | signin page started'

--- src/pages/signin/signIn.jsx
+++ src/pages/signin/signIn.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./signin.css";
 import logo from "../../images/logo.svg";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { GoSignIn } from "react-icons/go";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { MdEmail } from "react-icons/md";
 import { Link } from "react-router-dom";
@@ -35,87 +34,90 @@ function Signin() {
 
   return (
     <section id="signin">
-      <div className="signin-col1">
-        <img src={logo} alt="logo" />
+      <div className="signin-content">
+        <div className="signin-content-bg">
+        <img src={asset} alt="background" id="left-column-img" />
+        <h1>Come in and see all the new movies...</h1>
+        {/* <p>Lorem ipsum..</p> */}
+      </div>
+      </div>
 
-        <div className="links">
-          <Link to="/signup" className="sign-up-link">
-            New User?
-            <em> Sign up now</em>
-          </Link>
+      <div className="links">
+        <Link to="/signup" className="sign-up-link">
+          New User?
+          <span> Sign up now</span>
+        </Link>
+      </div>
+
+      <div className="col2">
+
+        <div className="signin-col1">
+          <img src={logo} alt="logo" />
         </div>
 
-        <div className="signin-content">
-          <img src={asset} alt="signin-left-pane" className="left-column-img" />
-          <h1>Come in and see all the new movies...</h1>
-          <p>Lorem ipsum..</p>
-        </div>
-
-        <div className="col2">
-          <h1 id="signin-header">Sign In</h1>
-          <form
-            className="form_signin"
-            onSubmit={handleSubmit(onSubmit)}
-            noValidate
-            autoComplete="off"
-          >
-            <div className="user_details">
-              <label className="signin-label">EMAIL ADDRESS</label>
-              <input
-                className="user-input"
-                type="email"
-                placeholder="Enter your username..."
-                {...register("email", {
-                  required: true,
-                  pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
-                })}
-              />
-              <MdEmail className="email_image" />
-              <error id="error">
-                {errors.email?.type === "required" && "*Enter email address"}
-              </error>
-            </div>
-
-            <label className="signin-label2">PASSWORD</label>
+        <h1 id="signin-header">Sign In</h1>
+        
+        <form
+          className="form_signin"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          autoComplete="off"
+        >
+          <div className="user_details">
+            <label className="signin-label">EMAIL ADDRESS</label>
             <input
-              className="user-input1"
-              type={type}
-              name="newPassword"
-              placeholder="Enter your password..."
-              {...register("newPassword", {
+              className="user-input"
+              type="email"
+              placeholder="Enter your username..."
+              {...register("email", {
                 required: true,
-                minLength: 5,
-                maxLength: 20,
+                pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
               })}
             />
-            <RiLockPasswordFill className="password_image" />
-            <span id="pwd_eye" onClick={handleToggle}>
-              <AiFillEye icon={icon} size={18} />
-            </span>
-
-            <error id="new">
-              {errors.newPassword?.type === "required" &&
-                "*Enter your password"}
+            <MdEmail className="email_image" />
+            <error id="error">
+              {errors.email?.type === "required" && "*Enter email address"}
             </error>
+          </div>
 
-            <div className="bottom">
-              <h3 id="remember">Remember me</h3>
-              <input type="checkbox" id="bottom-links-checkbox" />
+          <label className="signin-label2">PASSWORD</label>
+          <input
+            className="user-input1"
+            type={type}
+            name="newPassword"
+            placeholder="Enter your password..."
+            {...register("newPassword", {
+              required: true,
+              minLength: 5,
+              maxLength: 20,
+            })}
+          />
+          <RiLockPasswordFill className="password_image" />
+          <span id="pwd_eye" onClick={handleToggle}>
+            <AiFillEye icon={icon} size={18} />
+          </span>
 
-              <div className="bottom-links">
-                <Link to="/reset" className="forgotten">
-                  Forgotten Password?
-                </Link>
-              </div>
+          <error id="new">
+            {errors.newPassword?.type === "required" && "*Enter your password"}
+          </error>
+
+          <div className="bottom">
+            <h3 id="remember">Remember me</h3>
+            <input type="checkbox" id="bottom-links-checkbox" />
+
+            <div className="bottom-links">
+              <Link to="/reset" className="forgotten">
+                Forgotten Password?
+              </Link>
             </div>
+          </div>
 
-            <div>
-              <button className="button-signin" Link to="/home" type="submit">
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
+          <div>
+            <button className="button-signin" Link to="/home" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   );
--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -32,9 +32,8 @@ function Signup() {
 
   return (
     <section id="signup">
-      <div className="logo-signup">
-        <img src={logo} alt="logo" />
-        <div className="column" id="log-image">
+      <div className="left-column">
+        <div className="column">
           <img
             src={signinPoster}
             alt="signin-left-pane"
@@ -42,16 +41,26 @@ function Signup() {
           />
           <div className="content">
             <h1>Watch it all here first</h1>
-            <p>Lorem ipsum..</p>
+            {/* <p>Lorem ipsum..</p> */}
           </div>
         </div>
-      </div>
 
-      <form className="signup-form" onSubmit={handleSubmit(onSubmit)}>
+        <div></div>
+
         <Link to="/signin" className="signup-btn-link">
           Sign In
         </Link>
+      </div>
+
+      <div className="right-column">
+
+      <div className="logo-signup">
+          <img src={logo} alt="logo" />
+        </div>
+
+      <form className="signup-form" onSubmit={handleSubmit(onSubmit)}>
         <h3 id="createAccount">Create Account!</h3>
+
         <div className="signup-label" id="signup-textbox">
           <label className="user-email">EMAIL ADDRESS</label>
           <input
@@ -63,7 +72,7 @@ function Signup() {
               pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
             })}
           />
-          <error id='emailErr'>
+          <error id="emailErr">
             {errors.email?.type === "required" && "*Email is required"}
           </error>
         </div>
@@ -113,6 +122,7 @@ function Signup() {
           </button>
         </div>
       </form>
+      </div>
     </section>
   );
 }
